667235292fcd7dfc13f5b8846b244252d59c844a
mixelpixel <mixelpix@mac.com>
2017-06-15 10:08:12 -0400

    added numbers to problems

--- callbacks.js
+++ callbacks.js
@@ -16,7 +16,7 @@
  */
 
 
-// Write a function called firstItem that passes the first item of the given array to the callback function
+// 1) Write a function called firstItem that passes the first item of the given array to the callback function
 
 var foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
@@ -24,45 +24,45 @@ firstItem(foods, (firstItem) => {
   console.log('The first item is ' + firstItem);
 });
 
-// Write a function called getLength that passes the length of the array into the callback
+// 2) Write a function called getLength that passes the length of the array into the callback
 
 getLength(foods, (length) => {
   console.log('The length of the array is ' + length);
 });
 
-// Write a function called last which passes the last item of the array into the callback
+// 3) Write a function called last which passes the last item of the array into the callback
 
 last(foods, (lastItem) => {
   console.log('The last item in the array is ' + lastItem);
 });
 
-// Write a function called sumNums that adds two numbers and passes the result to the callback
+// 4) Write a function called sumNums that adds two numbers and passes the result to the callback
 
 sumNums(5, 10, (sum) => {
   console.log('The sum is ' + sum);
 });
 
-// Write a function called multiplyNums that adds two numbers and passes the result to the callback
+// 5) Write a function called multiplyNums that adds two numbers and passes the result to the callback
 
 multiplyNums(5, 10, (product) => {
   console.log('The product is ' + product);
 });
 
-// Write a function called contains that checks if an item is present inside of the given array.
+// 6) Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
 
 contains(foods, 'ribeye', (result) => {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
 });
 
-// Write a function called removeDuplicates that removes all duplicate values from the given array.
+// 7) Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
 removeDuplicates(foods, (uniqueFoods) => {
   console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
-// Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
+// 8) Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 
 forEach(foods, (value, index) => {
   console.log('${value} is at index ' + index);
