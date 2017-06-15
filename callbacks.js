2a476862fad0372d94ad533f2b1bab7b5eb13cac
mixelpixel <mixelpix@mac.com>
2017-06-15 10:14:32 -0400

    firstItem

--- callbacks.js
+++ callbacks.js
@@ -18,52 +18,57 @@
 
 // 1) Write a function called firstItem that passes the first item of the given array to the callback function
 
+var firstItem = (anArray, cb) => {
+  cb(anArray[0]);
+};
+
 var foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
 firstItem(foods, (firstItem) => {
   console.log('The first item is ' + firstItem);
 });
 
-// 2) Write a function called getLength that passes the length of the array into the callback
 
-getLength(foods, (length) => {
-  console.log('The length of the array is ' + length);
-});
-
-// 3) Write a function called last which passes the last item of the array into the callback
-
-last(foods, (lastItem) => {
-  console.log('The last item in the array is ' + lastItem);
-});
-
-// 4) Write a function called sumNums that adds two numbers and passes the result to the callback
-
-sumNums(5, 10, (sum) => {
-  console.log('The sum is ' + sum);
-});
-
-// 5) Write a function called multiplyNums that adds two numbers and passes the result to the callback
-
-multiplyNums(5, 10, (product) => {
-  console.log('The product is ' + product);
-});
-
-// 6) Write a function called contains that checks if an item is present inside of the given array.
-// Pass true to the callback if it is, otherwise pass false
-
-contains(foods, 'ribeye', (result) => {
-  console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
-});
-
-// 7) Write a function called removeDuplicates that removes all duplicate values from the given array.
-// Pass the array to the callback function.  Do not mutate the original array.
-
-removeDuplicates(foods, (uniqueFoods) => {
-  console.log('foods with duplicates removed: ' + uniqueFoods);
-});
-
-// 8) Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
-
-forEach(foods, (value, index) => {
-  console.log('${value} is at index ' + index);
-});
+// // 2) Write a function called getLength that passes the length of the array into the callback
+//
+// getLength(foods, (length) => {
+//   console.log('The length of the array is ' + length);
+// });
+//
+// // 3) Write a function called last which passes the last item of the array into the callback
+//
+// last(foods, (lastItem) => {
+//   console.log('The last item in the array is ' + lastItem);
+// });
+//
+// // 4) Write a function called sumNums that adds two numbers and passes the result to the callback
+//
+// sumNums(5, 10, (sum) => {
+//   console.log('The sum is ' + sum);
+// });
+//
+// // 5) Write a function called multiplyNums that adds two numbers and passes the result to the callback
+//
+// multiplyNums(5, 10, (product) => {
+//   console.log('The product is ' + product);
+// });
+//
+// // 6) Write a function called contains that checks if an item is present inside of the given array.
+// // Pass true to the callback if it is, otherwise pass false
+//
+// contains(foods, 'ribeye', (result) => {
+//   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
+// });
+//
+// // 7) Write a function called removeDuplicates that removes all duplicate values from the given array.
+// // Pass the array to the callback function.  Do not mutate the original array.
+//
+// removeDuplicates(foods, (uniqueFoods) => {
+//   console.log('foods with duplicates removed: ' + uniqueFoods);
+// });
+//
+// // 8) Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
+//
+// forEach(foods, (value, index) => {
+//   console.log('${value} is at index ' + index);
+// });
